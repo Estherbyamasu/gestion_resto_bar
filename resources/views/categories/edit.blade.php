9903fbc21571150996b942e3e74d41ad575a298a
esther byamasu <estherbyamasufuraha@gmail.com>
2020-08-27 01:04:05 -0700

    added commit index.blade.php

---
+++ resources/views/categories/edit.blade.php
@@ -0,0 +1,55 @@
+@extends('templates.default_layout')
+
+@section('content')
+
+<div class="modal-dialog form-dark" role="document">
+    <!--Content-->
+<div class="modal-content card-image" >
+      <div class="text-white  py-5 px-5 ">
+        <!--Header--><div class="modal-header text-center pb-4">
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+   
+    <!--/.row-->
+    <h3 class="modal-title w-100 white-text font-weight-bold" id="myModalLabel"><strong>Modification de la categories</strong> <a
+              class="green-text font-weight-bold"><strong> </strong></a></h3>
+              <a href="{{ url('categories') }}" > <button type="button" class="close white-text" data-dismiss="modal" aria-label="Close">
+            
+            <span aria-hidden="true">&times;</span></a>
+          </button></a>
+    
+    <!--/.row-->
+
+    <div class="row">
+        <div class="col-lg-10">
+            <div class="panel panel-default">
+                <div class="panel-heading">Modifier la categorie n&deg; {{ $category->id }}</div>
+                <div class="panel-body">
+                    <div class="col-md-12">
+                        <form role="form" action="/categories/{{ $category->id }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group">
+                                <label>Nom categorie:</label>
+                                <input type="text" name="cat_name" class="form-control" placeholder="Entrez le nom d'une categorie" value="{{ $category->cat_name }}" >
+                            </div>
+                            <div class="row">
+            <div class="text-center mb-3 col-md-4">
+              <button type="submit" class=" glyphicon glyphicon-edit btn btn-success btn-block btn-rounded z-depth-1">Modifier</button>
+              </div>
+             
+             </div>
+                        </form>
+                    </div>
+                </div>
+                </div>
+    </div>
+    </div>
+    </div>
+            </div><!-- /.panel-->
+        </div><!-- /.panel-->
+    </div><!-- /.col-->
+</div><!-- /.row -->
+</div>
+<!--/.main-->
+
+@endsection
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,102 @@
+@extends('templates.default_layout')
+
+@section('content')
+
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="#">
+                    <em class="fa fa-home"></em>
+                </a></li>
+            <li class="active">Categories</li>
+        </ol>
+    </div>
+    <!--/.row-->
+    
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Product categories</h1>
+        </div>
+    </div>
+    <!--/.row-->
+    
+  
+    <div class="row">
+        <div class="col-lg-10">
+            <div class="panel panel-default">
+                
+                <div class="panel-body">
+                    <div class="col-md-12">
+                    <div class="row">
+        <div class="col-lg-10">
+        <div class="modal-dialog modal-fluid modal-notify modal-info" role="document">
+    <div class="modal-content">
+      
+
+       
+            
+
+            <div class="panel panel-default">
+               
+                <div class="panel-body">
+                    <div class="col-md-8">
+                        <form role="form" action="{{ url('categories') }}" method="post">
+                            @csrf
+                            <div class="form-group">
+                                <label>Nom categorie:</label>
+                                <input type="text" name="cat_name" class="form-control" placeholder="Entrez le nom d'une categorie" required>
+                            </div>
+                            <div class="row">
+            <div class="text-center mb-3 col-md-6">
+              <button type="submit" class=" glyphicon glyphicon-plus btn  btn-primary btn-block btn-rounded z-depth-1">Save</button>
+              </div>
+              <div class="text-center mb-3 col-md-6">
+              <button  type="reset"class="btn btn-outline-info waves-effect ml-auto" data-dismiss="modal">Reset</button>
+              </div>
+             </div>
+                        </form>
+                    </div>
+                </div>
+                
+                </div>
+                </div>
+            </div><!-- /.panel-->
+        </div><!-- /.panel-->
+
+                        <table class="table table-bordered table-hover table-striped">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Id</th>
+                                    <th scope="col">Category Name</th>
+                                    <th scope="col">Options</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($categories as $category)
+                                <tr>
+                                    <td>{{ $category->id }}</td>
+                                    <td>{{ $category->cat_name }}</td>
+                                    <td>
+                                        <a href="categories/edit/{{ $category->id }}" class="glyphicon glyphicon-edit   btn btn-info">edit</a>
+                                        <a href="categories/show/{{$category->id}}" class="glyphicon glyphicon-play  btn btn-primary">Voir les produits</a>
+                                        <form action="categories/destroy/{{ $category->id }}" method="post" class="form-inline">
+                                        @csrf
+                                <button type="submit" onclick="return confirm('Voulez vs vraiment supprimer cette categorie ?')" class="glyphicon glyphicon-delite glyphicon-trash   btn btn-danger">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+            </div><!-- /.panel-->
+        </div><!-- /.panel-->
+    </div>
+</div><!-- /.row -->
+</div>
+<!--/.main-->
+
+@endsection
