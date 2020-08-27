f9712a4429a9089a352af72a0882a245cbb13f1d
esther byamasu <estherbyamasufuraha@gmail.com>
2020-08-27 06:39:24 -0700

    create.blade.php in ressources/views/detailleachats

---
+++ resources/views/detailleachats/create.blade.php
@@ -0,0 +1,26 @@
+@extends('templates.default_layout')
+@section('content')
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+    <div class="row">
+        <ol class="breadcrumb">
+            <li><a href="#">
+                    <em class="fa fa-home"></em>
+                </a></li>
+            <li class="active">Detaille Achats</li>
+        </ol>
+    </div>
+    <!--/.row-->
+
+   
+    <!--/.row-->
+
+
+    <div class="row">
+        <div class="col-lg-12">
+        </div><!-- /.panel-->
+    </div><!-- /.col-->
+    <div class="col-sm-12">
+        <p class="back-link">Lumino Theme by <a href="https://www.medialoot.com">Medialoot</a></p>
+    </div>
+</div><!-- /.row -->
+@endsection()
