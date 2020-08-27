1db0d5a2a08490aece8cf4c438745496b1869511
esther byamasu <estherbyamasufuraha@gmail.com>
2020-08-27 10:51:43 -0700

    create_detailleachat_table in database/migration

---
+++ database/migrations/2020_08_27_174625_create_detailleachats_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDetailleachatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detailleachats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('product_id');
+            $table->unsignedBigInteger('achat_id');
+            $table->integer('quantite');
+            $table->integer('prix');
+            $table->timestamps();
+            $table->foreign('product_id')
+                ->references('id')
+                ->on('products')
+                ->onDelete('cascade');
+            $table->foreign('achat_id')
+                ->references('id')
+                ->on('achats')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detailleachats');
+    }
+}
