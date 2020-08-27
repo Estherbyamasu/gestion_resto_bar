f94014b711997e0457e9b0b2fc503732d3acb386
esther byamasu <estherbyamasufuraha@gmail.com>
2020-08-27 10:54:13 -0700

    index.blade.php in ressources/views/detailleachats

--- resources/views/detailleachats/index.blade.php
+++ resources/views/detailleachats/index.blade.php
@@ -98,7 +98,7 @@
     </div>
     </div>
     </div>
-            <table class="table table-bordered table-hover table-striped">
+            <table id="example1" class="table table-bordered table-hover table-striped">
                 <thead>
                     <tr>
                         <th scope="col">ID</th>
